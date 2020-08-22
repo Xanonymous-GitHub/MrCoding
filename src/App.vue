2def88a9ac1d8308f083dd42261f1490310cf395
Xanonymous <trusaidlin@gmail.com>
2020-08-23 03:36:59 +0800

    upgrade packages and add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,20 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <router-view v-slot="{ Component }">
+    <keep-alive>
+      <component :is="Component"/>
+    </keep-alive>
+  </router-view>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+// import "@/assets/scss/app.scss";
+import {defineComponent} from '@vue/composition-api';
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default defineComponent({
+  name: "App",
+  props: {},
+  setup() {
+    return {};
   }
-}
-</style>
+});
+</script>
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import VueCompositionAPI from '@vue/composition-api'
 
 Vue.config.productionTip = false
+Vue.use(VueCompositionAPI)
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
